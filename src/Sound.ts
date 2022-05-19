07b0851ef5d45f7db7a64ae1253e05cff0419da6
ACitronella <phuriwat.13042@gmail.com>
2022-05-19 16:47:41 +0700

    messing with relative url

--- src/Sound.ts
+++ src/Sound.ts
@@ -1,15 +1,15 @@
 
 const SOUNDS = [
-    new Audio("./enna_voice/Beetch.mp3"),
-    new Audio("./enna_voice/Cough1.mp3"),
-    new Audio("./enna_voice/d_i_s_g_r_a_c_e.mp3"),
-    new Audio("./enna_voice/Enna_AreYouTrash.mp3"),
-    new Audio("./enna_voice/Enna_DisgustingHumanBeingsAreDisgusting.mp3"),
-    new Audio("./enna_voice/Enna_OogaBoogaBtch.mp3"),
-    new Audio("./enna_voice/Enna_ValleyGirl-FckingBtch.mp3"),
-    new Audio("./enna_voice/Feck_you_man_youre_banned_from_Nijisamazon.mp3"),
-    new Audio("./enna_voice/StopLivingInYourMothersBasement.mp3"),
-    new Audio("./enna_voice/Useless2.mp3")
+    new Audio("enna_voice/Beetch.mp3"),
+    new Audio("enna_voice/Cough1.mp3"),
+    new Audio("enna_voice/d_i_s_g_r_a_c_e.mp3"),
+    new Audio("enna_voice/Enna_AreYouTrash.mp3"),
+    new Audio("enna_voice/Enna_DisgustingHumanBeingsAreDisgusting.mp3"),
+    new Audio("enna_voice/Enna_OogaBoogaBtch.mp3"),
+    new Audio("enna_voice/Enna_ValleyGirl-FckingBtch.mp3"),
+    new Audio("enna_voice/Feck_you_man_youre_banned_from_Nijisamazon.mp3"),
+    new Audio("enna_voice/StopLivingInYourMothersBasement.mp3"),
+    new Audio("enna_voice/Useless2.mp3")
 ];
 
 export default SOUNDS;
